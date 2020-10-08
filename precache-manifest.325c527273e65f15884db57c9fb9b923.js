851bf33e08a8e4fe9198ef7edd7379bcb32ab3b3
NicolasAudepin <42012602+NicolasAudepin@users.noreply.github.com>
2020-10-09 01:04:07 +0200

    Updates

--- precache-manifest.325c527273e65f15884db57c9fb9b923.js
+++ precache-manifest.325c527273e65f15884db57c9fb9b923.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41094ecd3ab7e0324649abeb26baefdb",
+    "revision": "a9d912852f08211e56a857fb166603cb",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "d973d0fbe02cbd0e33eb",
+    "revision": "e1c410e5b4ffa249d64e",
     "url": "/portfolio/static/css/main.516de01f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.a04e1ee0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d973d0fbe02cbd0e33eb",
-    "url": "/portfolio/static/js/main.9a469b66.chunk.js"
+    "revision": "e1c410e5b4ffa249d64e",
+    "url": "/portfolio/static/js/main.3929b41c.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.be77482e04e97d1a1612c655a1822f78.js"
+  "/portfolio/precache-manifest.325c527273e65f15884db57c9fb9b923.js"
 );
 
 self.addEventListener('message', (event) => {
